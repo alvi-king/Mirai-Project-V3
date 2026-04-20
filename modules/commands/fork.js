a446db57ea6b4440c9cd8f0bdf4cb5efb8c186e2
ITZ ME ALVI KING <alviboss2026@gmail.com>
2026-04-21 00:34:48 +0300

    Add files via upload

---
+++ modules/commands/fork.js
@@ -0,0 +1,19 @@
+module.exports.config = {
+name: "fork",
+version: "1.0.0",
+hasPermssion: 0,
+credits: "ALVI",
+description: "Send GitHub repo link",
+commandCategory: "other",
+usages: "fork",
+cooldowns: 3,
+};
+
+module.exports.run = async function({ api, event }) {
+return api.sendMessage(
+"🔗 GitHub Repo Link:\n\nhttps://github.com/alvi-boss/ALVI-BOT-COMMUNITY.git",
+event.threadID,
+event.messageID
+);
+};
+
